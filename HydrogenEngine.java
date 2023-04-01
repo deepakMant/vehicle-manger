db78d15af96f5003c8642f51fcde52c2c2af77cb
Deepak Mantro <deepakmantro@gmail.com>
2023-04-01 15:02:46 +0530

    stop methd creted

--- HydrogenEngine.java
+++ HydrogenEngine.java
@@ -3,4 +3,8 @@ public class HydrogenEngine {
 	public void start() {
 		System.out.print("HydrogenEngine Petroal Engine");
 	}
+	
+	public void stop() {
+		System.out.print("Disel Engine Stoped");
+	}
 }
