b5f300d1834f89636bba4130da4081240a1fe1db
Deepak Mantro <deepakmantro@gmail.com>
2023-04-01 14:18:07 +0530

    Stop method added in EnecticEngine.java

---
+++ ElectricEngine.java
@@ -0,0 +1,10 @@
+public class ElectricEngine{
+	
+	public void start() {
+		System.out.print("Electric Engine Strat");
+	}
+	
+	public void stop() {
+		System.out.print("Electric Engine Stoped");
+	}
+}
